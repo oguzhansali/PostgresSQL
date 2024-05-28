31790bcbf50573d25facbe0fb3d86ae9a2959488
oguzhansali61 <154914330+oguzhansali61@users.noreply.github.com>
2024-05-28 22:59:21 +0300

    .

---
+++ src/Week5/ödev2.sql
@@ -0,0 +1,8 @@
+select * from film
+where replacement_cost between 12.99 AND 16.99;
+
+select first_name,last_name from actor
+where first_name IN ('Penelope','Nick','Ed');
+
+select * from film
+where rental_rate IN(0.99,2.99) and replacement_cost IN(12.99,15.99,28.99);
